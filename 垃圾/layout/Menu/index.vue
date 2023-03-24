a47ae8f1b9790b3e5f17783475bb9b0b0746b5f6
lllhhh123 <1027339115@qq.com>
2023-03-24 22:34:40 +0800

    提交4

---
+++ 垃圾/layout/Menu/index.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="Menu">
+      菜单区域
+    </div>
+  </template>
+  
+  <script setup lang="ts">
+  
+  </script>
+  <style lang='less' scoped>
+  .Menu{
+    width: 200px;
+    border-right: 1px solid #ccc;
+  }
+  </style>
